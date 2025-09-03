4e996b5ae0b11476101ab46d69c54ddc6bd2e73e
Winner Edwin <113316417+mikelneonedwin@users.noreply.github.com>
2025-09-03 21:19:38 +0100

    Require login to logout

--- routes/public.py
+++ routes/public.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, request, redirect, url_for, flash
-from flask_login import login_user, logout_user, current_user  # type: ignore
+from flask_login import login_user, logout_user, current_user, login_required  # type: ignore
 import models.db as db
 from models.user import User
 
@@ -110,6 +110,7 @@ def register():
 
 
 @public.route("/logout")
+@login_required
 def logout():
     logout_user()
     return redirect("/")
